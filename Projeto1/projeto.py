07447371963abdaa861ad08cb8696edf83ef902a
Ítalo Lucas Molino <112178610+TATHORbr@users.noreply.github.com>
2023-08-01 18:10:33 -0300

    Projeto - Olá Mundo
    primeiro projeto.

---
+++ Projeto1/projeto.py
@@ -0,0 +1,5 @@
+print('='*40)
+nome = input('Olá Jovem Gafanhoto. \nQual é o seu nome? ').capitalize().strip()
+print('\033[1;42mOlá {}!\033[m'.format(nome))
+print('Aqui iniciamos nossos projetos !\nVamos estudar Python AGORA')
+print('='*40)
