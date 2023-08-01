49e0348922ae05281dc65341f7cd5f27109b8d3c
Ítalo Lucas Molino <112178610+TATHORbr@users.noreply.github.com>
2023-08-01 18:20:47 -0300

    Update projeto.py

--- Projeto1/projeto.py
+++ Projeto1/projeto.py
@@ -1,7 +1,2 @@
-print('='*40)
 print('Olá Mundo !')
-
-nome = str(input('Olá Jovem Gafanhoto. \nQual é o seu nome? ')).capitalize().strip()
-print('\033[1;42mOlá {}!\033[m'.format(nome))
-print('Aqui iniciamos nossos projetos !\nVamos estudar Python AGORA.')
-print('='*40)
+print('Vamos programar.')
